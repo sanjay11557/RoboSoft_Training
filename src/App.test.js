15b0c81651175dd950fd8c3b07fec672977af722
Sanjay Pandey <sanjay.pandey1155@gmail.com>
2021-02-17 00:16:52 +0530

    test-CI

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { render, unmountComponentAtNode } from "react-dom";
 import { act } from "react-dom/test-utils";
-
 import App from "./App";
 
 let container = null;
